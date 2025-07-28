4468e76e8396627062690e5673d746f36639abe3
Yersmagit <169569860+Yersmagit@users.noreply.github.com>
2025-07-28 04:09:48 +0000

    chore(i18n): 更新翻译文件 (.ts) (Auto-Commit)
    - 触发事件: push
    - 提交哈希: 05a6b19fb9dc6848eaf9a5017aee77422622663c
    - 工作流运行: 16559818272
    
    * 由 GitHub Actions 自动生成
    
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- ui/default/i18n/zh_HK.ts
+++ ui/default/i18n/zh_HK.ts
@@ -87,6 +87,10 @@
     </message>
     <message>
         <location filename="../widget-countdown.ui" line="116"/>
+        <source>--:--</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../dark/widget-countdown.ui" line="116"/>
         <source>00:00</source>
         <translation>00:00</translation>
